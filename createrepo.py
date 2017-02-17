9957e775698af991774af70c2109cf7e44b85bc0
Brian O'Connor <briandoconnor@gmail.com>
2017-02-16 18:26:02 -0800

    need to get the tagging working, look at line 44

--- createrepo.py
+++ createrepo.py
@@ -4,11 +4,14 @@
 from github import Github
 import os
 import argparse
+from datetime import datetime, timedelta
 
 parser = argparse.ArgumentParser(description='Generate github repo and upload Dockerfile/CWL')
 parser.add_argument('--token', dest='token', help='your github token')
 parser.add_argument('--org', dest='org', help='the github organization to make repositories in')
 parser.add_argument('--tool', dest='tool', help='the tool being registered')
+parser.add_argument('--tag', action='append', dest='tag', help='the tags to apply to the repo')
+
 args = parser.parse_args()
 
 #g = Github("github handle", "password") #OR
@@ -23,15 +26,22 @@ try:
     repo = organization.create_repo(name=args.tool, description="Pfda2Dockstore Github repo for tool "+args.tool,
                             homepage="https://github.com", private=False,
                             has_issues=False, has_wiki=False, has_downloads=False )
-
-    # create files in repo
-    file_list = ['./Dockstore.cwl','./Dockerfile']
-    for entry in file_list:
-        with open(entry, 'rb') as input_file:
-            data = input_file.read()
-            file_path = '/'+ os.path.basename(input_file.name)
-            print (" + creating file:" + file_path)
-            repo.create_file(file_path, "initial commit", str(data))
+    # loop over each tag
+    # NOTE: you probably have different files for each tag!
+    for tag in args.tag:
+        # create files in repo
+        file_list = ['./Dockstore.cwl','./Dockerfile']
+        for entry in file_list:
+            with open(entry, 'rb') as input_file:
+                data = input_file.read()
+                file_path = '/'+ os.path.basename(input_file.name)
+                print (" + creating file:" + file_path)
+                repo.create_file(file_path, "initial commit", str(data))
+        since = datetime.now() - timedelta(days=1)
+        commits = repo.get_commits(since=since)
+        last = commits[0]
+        print (last)
+        repo.create_git_tag(tag, 'the tag message', last, 'commit')
 
 except:
-    print("errors creating org, check to ensure this is not a duplicate")
+    print("errors creating repo, check to ensure this is not a duplicate")
